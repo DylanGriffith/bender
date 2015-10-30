ec05554c334165a5541915956b158b5466dc83a7
Dylan Griffith <dyl.griffith@gmail.com>
2015-10-30 21:08:18 +1100

    ensure GenEvent is restarted after crashes

--- lib/bender/bot.ex
+++ lib/bender/bot.ex
@@ -6,12 +6,7 @@ defmodule Bender.Bot do
   end
 
   def init([config = %Matrix.Config{}, room_names, commands]) do
-    # Start the GenEvent manager
-    {:ok, event_manager} = GenEvent.start_link
-
-    Enum.each commands, fn(c) ->
-      :ok = GenEvent.add_mon_handler(event_manager, c, self)
-    end
+    event_manager = setup_event_manager(commands)
 
     # Login
     session = Matrix.Client.login!(config)
@@ -23,7 +18,7 @@ defmodule Bender.Bot do
 
     # Trigger first poll for events
     GenServer.cast(self, :poll_matrix)
-    {:ok, %{session: session, rooms: rooms, event_manager: event_manager, from: nil}}
+    {:ok, %{session: session, rooms: rooms, event_manager: event_manager, from: nil, commands: commands}}
   end
 
   def handle_cast(:poll_matrix, state = %{session: session = %Matrix.Session{}, event_manager: event_manager, from: from}) do
@@ -50,5 +45,23 @@ defmodule Bender.Bot do
     GenServer.cast(self, :poll_matrix)
 
     {:noreply, state}
+  end
+
+  def handle_info({:gen_event_EXIT, _, _}, state = %{commands: commands}) do
+    state = Dict.put(state, :event_manager, setup_event_manager(commands))
+    {:noreply, state}
+  end
+
+  def setup_event_manager(commands) do
+    # Start the GenEvent manager
+    {:ok, event_manager} = GenEvent.start_link
+
+    Process.monitor(event_manager)
+
+    Enum.each commands, fn(c) ->
+      :ok = GenEvent.add_mon_handler(event_manager, c, self)
+    end
+
+    event_manager
   end
 end
