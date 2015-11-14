43d13dba566481bd54d23770ebe3dbe584ca4f72
Dylan Griffith <dyl.griffith@gmail.com>
2015-11-14 17:18:11 +1100

    allow hyphens in commands

--- lib/bender/parser.ex
+++ lib/bender/parser.ex
@@ -1,6 +1,6 @@
 defmodule Bender.Parser do
   def try_parse(message, command_prefix \\ Application.get_env(:bender, :command_prefix)) do
-    match = Regex.named_captures(~r/^\s*@?#{command_prefix}:?\s+(?<command>\w+)\s*(?<message>.*)/i, message)
+    match = Regex.named_captures(~r/^\s*@?#{command_prefix}:?\s+(?<command>[\w-]+)\s*(?<message>.*)/i, message)
     if match && match["command"] do
       {:command, match["command"], match["message"]}
     else
--- test/bender/parser_test.exs
+++ test/bender/parser_test.exs
@@ -17,6 +17,10 @@ defmodule Bender.ParserTest do
     assert Bender.Parser.try_parse("Bender echo hello world") == {:command, "echo", "hello world"}
   end
 
+  test "#try_parse supports commands with hyphens" do
+    assert Bender.Parser.try_parse("Bender foo-bar hello world") == {:command, "foo-bar", "hello world"}
+  end
+
   test "#try_parse supports custom command_prefix" do
     assert Bender.Parser.try_parse("foobar echo hello world", "foobar") == {:command, "echo", "hello world"}
   end
