4e67138952f4fd081d331a4533df2084d766ae12
Daniel Flanagan <daniel@lytedev.io>
2018-10-17 17:07:28 -0500

    Multiline commands

--- lib/bender/parser.ex
+++ lib/bender/parser.ex
@@ -2,9 +2,10 @@ defmodule Bender.Parser do
   def try_parse(message, command_prefix \\ Application.get_env(:bender, :command_prefix)) do
     match =
       Regex.named_captures(
-        ~r/^\s*@?#{command_prefix}:?\s*(?<command>[\w-]+)\s*(?<message>.*)/i,
+        ~r/\s*@?#{command_prefix}:?\s*(?<command>[\w-]+)\s*(?<message>.*)/sim,
         message
       )
+      |> IO.inspect()
 
     if match && match["command"] do
       {:command, match["command"], match["message"]}
